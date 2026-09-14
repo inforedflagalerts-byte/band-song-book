0f3724b9c367c624aec155e0e001ec121976441a
inforedflagalerts-byte <info.redflag.alerts@gmail.com>
2026-09-14 15:32:01 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,131 @@
+const CACHE_NAME = "band-song-book-v1";
+
+const APP_FILES = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./app.js",
+    "./manifest.json"
+];
+
+
+self.addEventListener(
+    "install",
+    event => {
+
+        event.waitUntil(
+
+            caches.open(
+                CACHE_NAME
+            ).then(cache => {
+
+                return cache.addAll(
+                    APP_FILES
+                );
+
+            })
+
+        );
+
+        self.skipWaiting();
+
+    }
+);
+
+
+
+self.addEventListener(
+    "activate",
+    event => {
+
+        event.waitUntil(
+
+            caches.keys().then(
+                keys => {
+
+                    return Promise.all(
+
+                        keys
+                            .filter(
+                                key =>
+                                    key !== CACHE_NAME
+                            )
+                            .map(
+                                key =>
+                                    caches.delete(key)
+                            )
+
+                    );
+
+                }
+            )
+
+        );
+
+        self.clients.claim();
+
+    }
+);
+
+
+
+self.addEventListener(
+    "fetch",
+    event => {
+
+        const request =
+            event.request;
+
+
+        if (
+            request.method !== "GET"
+        ) {
+            return;
+        }
+
+
+        event.respondWith(
+
+            fetch(request)
+
+                .then(response => {
+
+                    if (
+                        response &&
+                        response.status === 200
+                    ) {
+
+                        const copy =
+                            response.clone();
+
+
+                        caches.open(
+                            CACHE_NAME
+                        ).then(cache => {
+
+                            cache.put(
+                                request,
+                                copy
+                            );
+
+                        });
+
+                    }
+
+
+                    return response;
+
+                })
+
+                .catch(() => {
+
+                    return caches.match(
+                        request
+                    );
+
+                })
+
+        );
+
+    }
+);
