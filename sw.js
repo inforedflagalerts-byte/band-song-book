e7049557f561468f3b6d21fe7e6c81d6908d1173
inforedflagalerts-byte <info.redflag.alerts@gmail.com>
2026-09-15 21:38:22 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "band-song-book-app-v5";
-const IMAGE_CACHE = "song-book-images-v5";
+const CACHE_NAME = "band-song-book-app-v4";
+const IMAGE_CACHE = "song-book-images-v4";
 
 const APP_FILES = [
     "./",
